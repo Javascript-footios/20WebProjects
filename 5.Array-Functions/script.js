0509a13babdd5379d23dc000f24af83e801ec75a
Fotios Tsakiris <footios76@gmail.com>
2021-06-06 20:50:46 +0300

    6.modal-dom-css

--- 5.Array-Functions/script.js
+++ 5.Array-Functions/script.js
@@ -93,7 +93,7 @@ function calculateWealth() {
   const result = data.reduce((acc, user) => acc + user.money, 0);
 
   const wealthEl = document.createElement("div");
-  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+  wealthEl.innerHTML = `<h3 class="total-wealth" >Total Wealth: <strong>${formatMoney(
     result
   )} </strong> </h3>`;
   main.appendChild(wealthEl);
